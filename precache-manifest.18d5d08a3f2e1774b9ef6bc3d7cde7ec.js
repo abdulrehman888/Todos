97488ded8270c750a3ce4f914f23172b2fc8f1f2
abdulrehman888 <khan.abdulrehmann@gmail.com>
2019-09-22 16:39:40 +0500

    Updates

---
+++ precache-manifest.18d5d08a3f2e1774b9ef6bc3d7cde7ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b47a8ba244e18cac729222b95909c9fb",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "d51d888cedfc9cd804d1",
+    "url": "/todos/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5ac59e2ffc0ff83d255d",
+    "url": "/todos/static/css/main.f3d5dd1d.chunk.css"
+  },
+  {
+    "revision": "d51d888cedfc9cd804d1",
+    "url": "/todos/static/js/2.a7bfc902.chunk.js"
+  },
+  {
+    "revision": "5ac59e2ffc0ff83d255d",
+    "url": "/todos/static/js/main.0c6e4b1e.chunk.js"
+  },
+  {
+    "revision": "72aeab8b15b7f527c74d",
+    "url": "/todos/static/js/runtime~main.7a88255b.js"
+  }
+]);
